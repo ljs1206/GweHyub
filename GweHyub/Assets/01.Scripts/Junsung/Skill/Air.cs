5e65daa0fa34eab94fc8478068842a04cf240ce1
ljs1206 <kunsung12345@gmail.com>
2023-11-08 00:14:24 +0900

    구슬치기 완성

--- GweHyub/Assets/01.Scripts/Junsung/Skill/Air.cs
+++ GweHyub/Assets/01.Scripts/Junsung/Skill/Air.cs
@@ -3,13 +3,39 @@ using UnityEngine;
 
 public class Air : MonoBehaviour
 {
+    private ParticleSystem gonggiEffect;
+    private CapsuleCollider2D capsuleCollider;
+
+    private void Awake()
+    {
+        gonggiEffect = transform.Find("gonggiEffect").GetComponent<ParticleSystem>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+    }
+
+    private void Start()
+    {
+        transform.localScale = Vector3.zero;
+        capsuleCollider.enabled = false;
+    }
+
+    private void Update()
+    {
+        #region DebugCode
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            capsuleCollider.enabled = true;
+            PlantAir();
+        }
+        #endregion
+    }
+
     public void PlantAir()
     {
-        transform.DOScale(1, 0.5f);
+        transform.DOScale(1, 0.3f).SetEase(Ease.InOutBounce);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        gonggiEffect.Play();
     }
 }
